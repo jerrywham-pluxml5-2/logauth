3b616268a4bbd49d6193b9a8f71b5bfad694df97
Cyril MAGUIRE <github@ecyseo.net>
2013-10-02 15:09:17 +0200

    Bug fixes

--- admin.php
+++ admin.php
@@ -24,17 +24,25 @@ if (isset($_GET['history']) && $_GET['history'] == true) {
 </style>
 <ul>
 	<li>
-		<a href="<?php echo PLX_CORE ?>admin/plugin.php?p=logAuth&rec=true">Archiver les logs</a>
+		<a href="<?php echo PLX_CORE ?>admin/plugin.php?p=logauth&rec=true">Archiver les logs</a>
 	</li>
 	<li>
-		<a href="<?php echo PLX_CORE ?>admin/plugin.php?p=logAuth&history=true">Historique des logs</a>
+		<a href="<?php echo PLX_CORE ?>admin/plugin.php?p=logauth&history=true">Historique des logs</a>
 	</li>
 	<li>
 		----------------------------------------
 	</li>
 	<li>
-		<a href="<?php echo PLX_CORE ?>admin/plugin.php?p=logAuth&purge=true" onclick="return confirm('Êtes-vous sûr de vouloir supprimer ces archives ? Cette action est irréversible !');">Purger l'historique des logs</a>
+		<a href="<?php echo PLX_CORE ?>admin/plugin.php?p=logauth&purge=true" onclick="return confirm('Êtes-vous sûr de vouloir supprimer ces archives ? Cette action est irréversible !');">Purger l'historique des logs</a>
 	</li>
 </ul>
 <h1>Fichier de log</h1>
 <?php }?>
+<pre>02-10-2013 @ 15:07 ::1
+Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:24.0) Gecko/20130802 Firefox/24.0
+Array
+(
+)
+
+Admin disconnect
+</pre>
